a45d75ac6090cb85bfff50cfd2e093a2ec32ec73
José Flores <josefloressv@gmail.com>
2025-08-20 00:52:46 -0600

    Add common tags for event, cost allocation, and workload in locals.tf

--- terraform/core/locals.tf
+++ terraform/core/locals.tf
@@ -4,6 +4,9 @@ locals {
   public_subnets_cidr  = [var.public_subnet1_cidr, var.public_subnet2_cidr]
   private_subnets_cidr = [var.private_subnet1_cidr, var.private_subnet2_cidr]
   common_tags = {
+    Event          = "KCDColombia2025"
+    CostAllocation = "Direct"
+    Workload       = "ECS"
     Platform       = var.platform
     Environment    = var.environment
     Provisioned_by = "Terraform"
