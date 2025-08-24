d4e911b17fa1996f8b09da111d324597156c2b72
José Flores <josefloressv@gmail.com>
2025-08-24 17:18:14 -0600

    Add ExternalDNS IAM role and ServiceAccount configuration

--- terraform/eks/helm_release.tf
+++ terraform/eks/helm_release.tf
@@ -6,18 +6,18 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   version    = var.argocd_chart_version
 
-  namespace = "argocd"
-
+  namespace        = "argocd"
   create_namespace = true
-
-  set = [{
-    name  = "server.service.type"
-    value = "LoadBalancer"
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
     },
     {
       name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
       value = "nlb"
-  }]
+    }
+  ]
 }
 
 # Helm release del AWS Load Balancer Controller
@@ -28,8 +28,6 @@ resource "helm_release" "aws_load_balancer_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
   version    = "1.8.1"
-
-  # Usamos el SA existente (IRSA)
   set = [
     {
       name  = "serviceAccount.create"
@@ -51,14 +49,67 @@ resource "helm_release" "aws_load_balancer_controller" {
       name  = "vpcId"
       value = module.vpc.vpc_id
     }
-    # (Opcional) Si usas IngressClass por defecto:
-    # {
+    # ,{
     #   name  = "defaultIngressClass.enabled"
     #   value = "true"
     # }
   ]
 
-  depends_on = [
-    kubernetes_service_account.alb_sa
+  depends_on = [kubernetes_service_account.alb_sa]
+}
+
+###############################################################################
+# Helm release ExternalDNS
+# Docs: https://github.com/kubernetes-sigs/external-dns
+###############################################################################
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+  version    = "1.15.0" # ajustar si se requiere
+
+  depends_on = [module.eks, kubernetes_service_account.externaldns_sa]
+  set = [
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = kubernetes_service_account.externaldns_sa.metadata[0].name
+    },
+    {
+      name  = "provider"
+      value = "aws"
+    },
+    {
+      name  = "policy"
+      value = "upsert-only"
+    },
+    {
+      name  = "domainFilters[0]"
+      value = var.domain_name
+    },
+    {
+      name  = "registry"
+      value = "txt"
+    },
+    {
+      name  = "txtOwnerId"
+      value = local.name_prefix
+    },
+    {
+      name  = "interval"
+      value = "1m"
+    },
+    {
+      name  = "logLevel"
+      value = "info"
+    },
+    {
+      name  = "triggerLoopOnEvent"
+      value = "true"
+    }
   ]
 }
--- terraform/eks/irsa.tf
+++ terraform/eks/irsa.tf
@@ -35,3 +35,41 @@ resource "kubernetes_service_account" "alb_sa" {
   }
   automount_service_account_token = true
 }
+
+###############################################################################
+# IRSA: rol IAM para ExternalDNS
+###############################################################################
+module "externaldns_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.39"
+
+  role_name          = join("-", [local.name_prefix, "externaldns-irsa"])
+  policy_name_prefix = "${local.name_prefix}-"
+
+  attach_external_dns_policy = true
+
+  external_dns_hosted_zone_arns = [
+    "arn:aws:route53:::hostedzone/${var.route53_zone_id}"
+  ]
+
+  oidc_providers = {
+    this = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "externaldns_sa" {
+  metadata {
+    name      = "external-dns"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.externaldns_irsa.iam_role_arn
+    }
+    labels = {
+      "app.kubernetes.io/name" = "external-dns"
+    }
+  }
+  automount_service_account_token = true
+}
